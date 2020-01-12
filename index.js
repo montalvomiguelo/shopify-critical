d86b80b2a4268e05aa3fd31584c901163af179b2
Montalvo Miguelo <montalvomiguelo@yahoo.com>
2020-01-12 01:46:54 -0800

    Inline critical css

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require('fs')
+const path = require('path')
 const yaml = require('js-yaml')
 const { extractCritical } = require('./lib/extract-critical')
+const inlineCritical = require('./lib/inline-critical')
 
 const criticalConfigFile = fs.readFileSync('shopify_critical.yml', 'utf8')
 const themekitConfigFile = fs.readFileSync('config.yml', 'utf8')
@@ -22,8 +24,12 @@ const penthouseOptions = {
   height
 }
 
+const stylesheet = path.basename(css)
+
 async function shopifyCritical () {
   const criticalCSS = await extractCritical(urls, penthouseOptions)
+
+  inlineCritical(target, criticalCSS, stylesheet)
 }
 
 module.exports = shopifyCritical
