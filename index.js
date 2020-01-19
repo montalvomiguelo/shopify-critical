0bdb702b2b2395b9cb6560e4816b5a9be930c744
Montalvo Miguelo <montalvomiguelo@yahoo.com>
2020-01-18 18:56:04 -0800

    Upload stylesheet and target liquid file to Shopify

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const yaml = require('js-yaml')
 const { extractCritical } = require('./lib/extract-critical')
 const inlineCritical = require('./lib/inline-critical')
 const replaceAsset = require('./lib/replace-asset')
+const deploy = require('./lib/deploy')
 
 const criticalConfigFile = fs.readFileSync('shopify_critical.yml', 'utf8')
 const themekitConfigFile = fs.readFileSync('config.yml', 'utf8')
@@ -17,7 +18,9 @@ const {
   height = 900,
   target = '',
   urls = [],
-  minify = false
+  minify = false,
+  base = '',
+  env = ''
 } = criticalConfig
 
 const penthouseOptions = {
@@ -29,11 +32,15 @@ const penthouseOptions = {
 const stylesheet = path.basename(css)
 
 async function shopifyCritical () {
+  const files = [css, target]
+  const config = themekitConfig[env]
   const criticalCSS = await extractCritical(urls, penthouseOptions)
 
   inlineCritical(target, criticalCSS, stylesheet)
 
   replaceAsset(css, criticalCSS, minify)
+
+  deploy(files, base, config, env)
 }
 
 module.exports = shopifyCritical
